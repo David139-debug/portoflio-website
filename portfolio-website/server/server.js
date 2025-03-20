029079431bf0d429df163bd256c340db60651e15
David139-debug <davidmalesevic55@gmail.com>
2025-03-20 20:18:05 +0100

    Update server.js

--- portfolio-website/server/server.js
+++ portfolio-website/server/server.js
@@ -111,7 +111,6 @@ io.on("connection", (socket) => {
     userId === adminId ? isAdminActive = true : "";
 
     users[userId] = socket.id;
-    delete users;
 
     if (users[adminId]) {
         io.to(users[adminId]).emit("updateUsers", Object.keys(users));
